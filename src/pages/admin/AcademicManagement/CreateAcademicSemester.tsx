4db352e91389ad5ccce844e9aba363e1bbf8f69a
saifulislam106 <saifulislam106915@gmail.com>
2025-01-20 23:57:49 +0600

    academic semester form added

--- src/pages/admin/AcademicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/AcademicManagement/CreateAcademicSemester.tsx
@@ -2,6 +2,7 @@ import { FieldValues, SubmitHandler } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
 import PHInput from "../../../components/form/PHInput";
 import { Button, Col, Flex } from "antd";
+import PHSelect from "../../../components/form/PHSelect";
 
 
 const CreateAcademicSemester = () => {
@@ -12,8 +13,11 @@ const CreateAcademicSemester = () => {
         <Flex justify="center" align="middle">
         <Col span={6}>
         <PHForm onSubmit={onSubmit}>
-            <PHInput type="text" name="name" />
+            <PHInput type="text" name="name" label="Name:" />
+            <PHSelect />
+            <div>
             <Button htmlType="submit">Submit</Button>
+            </div>
         </PHForm>
         </Col>
         </Flex>
