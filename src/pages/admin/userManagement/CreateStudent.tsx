eb95dbf55a3c5a3b38d00f4607b0b70af934eeb5
saifulislam106 <saifulislam106915@gmail.com>
2025-01-26 11:24:18 +0600

    create student form layout done

--- src/pages/admin/userManagement/CreateStudent.tsx
+++ src/pages/admin/userManagement/CreateStudent.tsx
@@ -1,58 +1,149 @@
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
 import PHInput from "../../../components/form/PHInput";
-import { Button } from "antd";
+import { Button, Col, Divider, Row } from "antd";
 
 const studentData = {
+  password: "student123",
+  student: {
+    name: {
+      firstName: "I am ",
+      middleName: "Student",
+      lastName: "Number 1",
+    },
+    gender: "male",
+    dateOfBirth: "1990-01-01",
+    bloogGroup: "A+",
 
-"password": "student123",
-    "student": {
-        "name": {
-            "firstName": "I am ",
-            "middleName": "Student",
-            "lastName": "Number 1"
-        },
-        "gender": "male",
-        "dateOfBirth": "1990-01-01",
-        "email": "student2@gmail.com",
-        "contactNo": "1235678",
-        "emergencyContactNo": "987-654-3210",
-        "bloogGroup": "A+",
-        "presentAddress": "123 Main St, Cityville",
-        "permanentAddress": "456 Oak St, Townsville",
-        "guardian": {
-            "fatherName": "James Doe",
-            "fatherOccupation": "Engineer",
-            "fatherContactNo": "111-222-3333",
-            "motherName": "Mary Doe",
-            "motherOccupation": "Teacher",
-            "motherContactNo": "444-555-6666"
-        },
-        "localGuardian": {
-            "name": "Alice Johnson",
-            "occupation": "Doctor",
-            "contactNo": "777-888-9999",
-            "address": "789 Pine St, Villageton"
-        },
-        "admissionSemester": "65b0104110b74fcbd7a25d92",
-        "academicDepartment": "65b00fb010b74fcbd7a25d8e"
-    }
-}
+    email: "student2@gmail.com",
+    contactNo: "1235678",
+    emergencyContactNo: "987-654-3210",
+    presentAddress: "123 Main St, Cityville",
+    permanentAddress: "456 Oak St, Townsville",
 
+
+    guardian: {
+      fatherName: "James Doe",
+      fatherOccupation: "Engineer",
+      fatherContactNo: "111-222-3333",
+      motherName: "Mary Doe",
+      motherOccupation: "Teacher",
+      motherContactNo: "444-555-6666",
+    },
+
+    localGuardian: {
+      name: "Alice Johnson",
+      occupation: "Doctor",
+      contactNo: "777-888-9999",
+      address: "789 Pine St, Villageton",
+    },
+
+    admissionSemester: "65b0104110b74fcbd7a25d92",
+    academicDepartment: "65b00fb010b74fcbd7a25d8e",
+  },
+};
 
 const CreateStudent = () => {
-    const onSublit:SubmitHandler<FieldValues> =(data)=>{
-        console.log(data);
-        const formData = new FormData()
-        formData.append("data" , JSON.stringify(data))
-        console.log(Object.fromEntries(formData));
-    }
-    return (
+  const onSublit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data);
+
+    const formData = new FormData();
+    formData.append("data", JSON.stringify(data));
+    console.log(Object.fromEntries(formData));
+  };
+  return (
+    <Row>
+      <Col span={24}>
         <PHForm onSubmit={onSublit}>
-            <PHInput type="text" name= "name" label="Name"  />
-            <Button htmlType="submit">Submit</Button>
+          <Row gutter={8}>
+          <Divider>Personal Info</Divider>
+          <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="name.firstName" label="First Name" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="name.middleName" label="Middle Name" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="name.lastName:" label="Last Name" />
+            </Col>
+          <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="gender" label="Gender" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="dateOfBirth" label="Date Of Birth" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="bloogGroup" label="Blood Group" />
+            </Col>
+
+            <Divider>Contact Info</Divider>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="email" label="Email" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="contactNo" label="Contact No" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="emergencyContactNo" label="Emergency Contact No" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="presentAddress" label="Present Address" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="permanentAddress" label="Permanent
+              Address" />
+            </Col>
+
+            <Divider>Guardian Info</Divider>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="guardian.fatherName" label="Father Name" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="guardian.fatherOccupation" label="Father Occupation" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="guardian.fatherContactNo" label="Father ContactNo" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="guardian.motherName" label="Mother Name" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="guardian.motherOccupation" label="Mother Occupation" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="guardian.motherContactNo" label="Mother
+              ContactNo" />
+            </Col>
+
+            <Divider>Local Guardian Info</Divider>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="localGuardian.name" label="Name" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="localGuardian.occupation" label="Occupation" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="localGuardian.contactNo" label="Contact No" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="localGuardian.address" label="Address" />
+            </Col>
+
+
+            <Divider>Academic Info</Divider>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="admissionSemester" label="Admission Semester" />
+            </Col>
+            <Col span={24} md={{span:12}} lg={{span:8}}>
+              <PHInput type="text" name="academicDepartment" label="Academic Department" />
+            </Col>
+
+          </Row>
+          <Button htmlType="submit">Submit</Button>
         </PHForm>
-    );
+      </Col>
+    </Row>
+  );
 };
 
 export default CreateStudent;
